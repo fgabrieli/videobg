1901a6e0ca8d15bd96c1f6e0b71719ad986bb284
fgabriel <fgabriel@fgabriel-ThinkPad-T430>
2016-06-09 17:44:27 -0300

    Making it oop friendly :)

--- js/app.js
+++ js/app.js
@@ -1,94 +1,183 @@
+// Video classes
+var videoTypes = [ YoutubeVideo, /* VimeoVideo */];
 
-function getVimeoIframe(videoUrl) {
-    return $('<iframe src="'
-            + videoUrl
-            + '?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+// Abstract
+function Video($targetEl, videoUrl) {
+    this.$targetEl = $targetEl;
+
+    this.videoUrl = videoUrl;
+
+    this.$iframe = {};
+
+    var t = this;
+    $(window).resize(function() {
+        t.reload();
+    })
 }
 
-function getYoutubeIframe(videoUrl) {
-    return $('<iframe width="560" height="315" src="' + videoUrl
-            + '?autoplay=true&controls=0" frameborder="0" allowfullscreen></iframe>');
-}
+$.extend(Video.prototype, {
+    targetMiddlePart : function($containerEl) {
+        var deltaY = this.videoHeight / 2;
+        deltaY -= this.$targetEl.height() / 2;
+        deltaY = -1 * deltaY;
 
-function setVideoBg($ndEl, videoUrl) {
-    var $iframe = {};
-    if (videoUrl.match(/^https:\/\/(www\.)?youtube\.com/) != null) {
-        $iframe = getYoutubeIframe(videoUrl);
-    } else if (videoUrl.match(/^https:\/\/player\.vimeo\.com/) != null) {
-        $iframe = getVimeoIframe(videoUrl);
-    } else {
-        throw 'Video url is not valid';
-    }
+        $containerEl.css('top', deltaY + 'px');
+
+        var deltaX = this.videoWidth / 2;
+        deltaX -= this.$targetEl.width() / 2;
+        deltaX = -1 * deltaX;
+
+        $containerEl.css('left', deltaX + 'px');
+    },
+
+    render : function() {
+        this.videoUrl = this.getEmbedUrl();
+
+        this.createIframe();
+
+        this.adjustSize();
+
+        // video container
+        $videoContainerEl = $('<div class="video-background">');
+        this.$iframe.appendTo($videoContainerEl);
+
+        // set overlay so the video can not be clicked/hovered
+        var $overlayEl = $('<div class="video-background-overlay">');
+        $overlayEl.appendTo(this.$targetEl);
+
+        // append video bg to element
+        $videoContainerEl.appendTo(this.$targetEl);
+
+        // target the middle part of the video
+        this.targetMiddlePart($videoContainerEl);
+    },
+
+    remove : function() {
+        this.$targetEl.find('.video-background').remove();
+        this.$targetEl.find('.video-background-overlay').remove();
+    },
     
-    var $videoBg = $('<div class="video-background">');
-    $iframe.appendTo($videoBg);
-
-    // adjust iframe size to match 16:9 aspect ratio
-    var videoWidth = $ndEl.width();
-    $iframe.css('width', videoWidth + 'px');
-
-    var videoHeight = (9 / 16 * videoWidth);
-    $iframe.css('height', videoHeight + 'px')
-
-    // set overlay so the video can not be clicked/hovered
-    var $overlay = $('<div class="video-background-overlay">');
-    $overlay.appendTo($ndEl);
-
-    // append video bg to element
-    $videoBg.appendTo($ndEl);
-
-    // target the middle part of the video
-    var deltaY = videoHeight / 2;
-    deltaY -= $ndEl.height() / 2;
-    deltaY = -1 * deltaY;
-
-    var deltaX = videoWidth / 2;
-    deltaX -= $ndEl.width() / 2;
-    deltaX = -1 * deltaX;
-
-    $videoBg.css('top', deltaY + 'px');
-    $videoBg.css('left', deltaX + 'px');
-
-}
-
-function setYoutubeVideo($ndEl, videoUrl) {
-    var $videoBg = $('<div class="video-background">');
-
-    $iframe.appendTo($videoBg);
-
-    // adjust iframe size to match 16:9 aspect ratio
-    var videoWidth = $ndEl.width();
-    $iframe.css('width', videoWidth + 'px');
-
-    var videoHeight = (9 / 16 * videoWidth);
-    $iframe.css('height', videoHeight + 'px')
-
-    // set overlay so the video can not be clicked/hovered
-    var $overlay = $('<div class="video-background-overlay">');
-    $overlay.appendTo($ndEl);
-
-    // append video bg to element
-    $videoBg.appendTo($ndEl);
-
-    // target the middle part of the video
-    var deltaY = videoHeight / 2;
-    deltaY -= $ndEl.height() / 2;
-    deltaY = -1 * deltaY;
-
-    var deltaX = videoWidth / 2;
-    deltaX -= $ndEl.width() / 2;
-    deltaX = -1 * deltaX;
-
-    $videoBg.css('top', deltaY + 'px');
-    $videoBg.css('left', deltaX + 'px');
-}
-
-function unsetVideoBg($ndEl) {
-    $ndEl.find('.video-background').remove();
-    $ndEl.find('.video-background-overlay').remove();
-}
-
-$(document).ready(function() {
-    var $ndEl = $('.nd-el:nth(0)');
-    setVideoBg($ndEl, 'hssssttps://player.vimeo.com/visssdeo/164437997');
+    reload : function() {
+        this.remove();
+        
+        this.render();
+    }
 })
+
+// Youtube
+function YoutubeVideo($targetEl, videoUrl) {
+    Video.call(this, $targetEl, videoUrl);
+}
+
+$.extend(YoutubeVideo.prototype, Video.prototype, {
+    createIframe : function() {
+        this.$iframe = $('<iframe width="560" height="315" src="' + this.videoUrl
+                + '?autoplay=1&loop=1&controls=0" frameborder="0" allowfullscreen></iframe>');
+    },
+    
+    adjustSize : function() {
+        this.videoWidth = screen.width;
+
+        this.videoHeight = (9 / 16 * this.videoWidth);
+
+        if (this.videoHeight < this.$targetEl.height()) {
+            this.videoHeight = this.$targetEl.height();
+
+            // recalc video width
+            this.videoWidth = (16 / 9 * this.videoHeight);
+        }
+
+        // apply dimensions to iframe
+        this.$iframe.css('width', this.videoWidth + 'px');
+        this.$iframe.css('height', this.videoHeight + 'px')
+    },
+
+    getEmbedUrl : function() {
+        var watchType = this.videoUrl.match(/\/watch\?v=(.*)$/);
+        if (watchType !== null) {
+            var videoId = watchType[1];
+            url = 'https://www.youtube.com/embed/' + videoId;
+        }
+
+        return url;
+    },
+
+    // static
+    isValid : function(url) {
+        return url.match(/^https:\/\/(www\.)?youtube\.com/) != null;
+    }
+});
+
+// Vimeo
+function VimeoVideo($targetEl, videoUrl) {
+    Video.call(this, $targetEl, videoUrl);
+}
+
+$.extend(VimeoVideo.prototype, Video.prototype, {
+    createIframe : function() {
+        this.$iframe = $('<iframe src="'
+                + videoUrl
+                + '?autoplay=1&loop=1&color=transparent&title=0&byline=0&portrait=0&badge=0&byline=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+    },
+    
+    adjustSize : function() {
+        // 1.06 = 6%. We need to add this width to hide vimeo
+        // controls
+        this.videoWidth = 1.06 * screen.width;
+
+        this.videoHeight = (9 / 16 * this.videoWidth);
+
+        var elHeight = $el.height();
+
+        // 1.08 = 8%. We need to add this width to hide vimeo
+        // controls
+        var heightToHideControls = elHeight + (elHeight * 1.08);
+
+        if (this.videoHeight < heightToHideControls) {
+            this.videoHeight = heightToHideControls;
+
+            // recalc video width
+            this.videoWidth = (16 / 9 * this.videoHeight);
+        }
+
+        // apply dimensions to iframe
+        this.$iframe.css('width', this.videoWidth + 'px');
+        this.$iframe.css('height', this.videoHeight + 'px')
+    },
+
+    getEmbedUrl : function() {
+        var lastPart = url.match(/\/(\d+)$/);
+        if (lastPart == null) {
+            throw 'Vimeo url is not valid';
+        }
+
+        var videoId = lastPart[1];
+        url = '//player.vimeo.com/video/' + videoId;
+
+        return url;
+    },
+
+    // static
+    isValid : function(url) {
+        return (url.match(/\/\/.*?vimeo\.com/) != null);
+    }
+});
+
+{
+    var videos = [];
+
+    $(document).ready(function() {
+
+        var $el = $('.nd-el:nth(0)');
+
+        var videoUrl = 'https://www.youtube.com/watch?v=6pxRHBw-k8M';
+
+        for (var i = 0; i < videoTypes.length; i++) {
+            var videoType = videoTypes[i];
+            if (videoType.prototype.isValid(videoUrl)) {
+                videos.push(new videoType($el, videoUrl));
+                videos[i].render();
+            }
+        }
+    });
+}
